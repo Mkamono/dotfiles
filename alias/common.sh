1bdaba00c816371bf9c3504fad26d92c8c7e6124
Mkamono <chengyaye31@gmail.com>
2023-10-14 06:34:31 +0000

    update

---
+++ alias/common.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+alias c="clear"
+alias e="exit"
+alias ..="cd .."
+alias ...="cd ../.."
+alias .2="cd ../../"
+alias .3="cd ../../../"
+alias .4="cd ../../../../"
+alias .5="cd ../../../../../"
+
+alias ls="exa"
+alias la="exa -a"
+alias ll="exa -l"
+alias tr="exa -T"
+alias ta="exa -Ta"
+alias rmrf="rm -rfi"
+
+echo -e "✨ \e[36;1menabled alias\e[m"
---
+++ prompt.sh
@@ -0,0 +1,2 @@
+source ~/.git-prompt.sh
+PS1='\n \[\e[33m\]\w\[\e[1;36m\]$(__git_ps1 " (%s)")\[\e[0m\] >'
