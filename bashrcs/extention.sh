9569f065bddb564dd3408e36b316b5fa50aa0b32
Mkamono <chengyaye31@gmail.com>
2023-10-16 02:27:54 +0900

    change extention installation

--- bashrcs/extention.sh
+++ bashrcs/extention.sh
@@ -1,28 +1,25 @@
 #! /bin/bash
 
 function cmext() {
-    code --install-extension ms-azuretools.vscode-docker
-    code --install-extension GitHub.copilot
-    code --install-extension GitHub.copilot-chat
-    code --install-extension cweijan.vscode-database-client2
-    code --install-extension rangav.vscode-thunder-client
-    code --install-extension streetsidesoftware.code-spell-checker
-    code --install-extension eamodio.gitlens
-    code --install-extension GitHub.vscode-pull-request-github
-    code --install-extension redjue.git-commit-plugin
-    code --install-extension mhutchie.git-graph
-    code --install-extension usernamehw.errorlens
+    cd dotfiles || return
+    for item in $(jq -r '.extensions.common[]' extensions.json); do
+        code --install-extension "$item"
+    done
+    cd ..
 }
 
 function pyext() {
-    code --install-extension ms-python.python
-    code --install-extension ms-toolsai.jupyter
-    code --install-extension njpwerner.autodocstring
-    code --install-extension ms-python.black-formatter
-    code --install-extension ms-python.isort
+    cd dotfiles || return
+    for item in $(jq -r '.extensions.python[]' extensions.json); do
+        code --install-extension "$item"
+    done
+    cd ..
 }
 
 function shext() {
-    code --install-extension timonwong.shellcheck
-    code --install-extension foxundermoon.shell-format
+    cd dotfiles || return
+    for item in $(jq -r '.extensions.shell[]' extensions.json); do
+        code --install-extension "$item"
+    done
+    cd ..
 }
