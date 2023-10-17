35376a9e75b83a48f4876bf48b60c5bffc2d14aa
Mkamono <chengyaye31@gmail.com>
2023-10-17 04:36:31 +0000

    update

--- bashrcs/gcloud.sh
+++ bashrcs/gcloud.sh
@@ -17,8 +17,8 @@ function gaj() {
 }
 
 function install_cloud_sql_proxy() {
-    curl -o ~/cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.7.0/cloud-sql-proxy.linux.amd64
-    chmod +x ~/cloud-sql-proxy
+    curl -o "${workspace_dir}"/cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.7.0/cloud-sql-proxy.linux.amd64
+    chmod +x "${workspace_dir}"/cloud-sql-proxy
 }
 
 function start_cloud_sql_proxy() {
@@ -26,7 +26,7 @@ function start_cloud_sql_proxy() {
         read -p "SQL接続名を入力してください : " INPUT_STR
         export INSTANCE_CONNECTION_NAME=$INPUT_STR
     fi
-    ~/cloud-sql-proxy "$INSTANCE_CONNECTION_NAME"
+    "${workspace_dir}"/cloud-sql-proxy "$INSTANCE_CONNECTION_NAME"
 }
 
 function gcloud_setup(){
