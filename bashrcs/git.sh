174ade35411c3d797da0b9bf19dfc98ea59853d1
Mkamono <chengyaye31@gmail.com>
2023-11-06 14:23:14 +0900

    add specify default branch name

--- bashrcs/git.sh
+++ bashrcs/git.sh
@@ -2,6 +2,8 @@
 
 source ~/.git-completion.sh
 
+default_branch=$(git branch -l | cut -d ' ' -f 2)
+
 function gd() {
     git fetch --prune
     number_of_branches=$(git branch | wc -l)
@@ -9,11 +11,11 @@ function gd() {
     if [ "${number_of_branches}" -eq 1 ]; then
         echo 'only one branch'
     else
-        if [ "${current_branch}" = 'main' ]; then
-            git branch | grep -v 'main' | xargs git branch -D
+        if [ "${current_branch}" = "${default_branch}" ]; then
+            git branch | grep -v "${default_branch}" | xargs git branch -D
         else
             git checkout main &&
-                git branch | grep -v 'main' | xargs git branch -d &&
+                git branch | grep -v "${default_branch}" | xargs git branch -d &&
                 git checkout "${current_branch}"
         fi
     fi
